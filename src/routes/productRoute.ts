06de18fe0537643b974afb027cf456b58124f0e9
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-11-08 23:53:18 -0500

    complete product controller

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,1 +1,18 @@
-"Use client";
+import express from "express";
+import {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController";
+
+const router = express.Router();
+
+router.post("/create-product", createProduct);
+router.get("/products", getAllProducts);
+router.get("/product/:id", getSingleProduct);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
+
+export default router;
