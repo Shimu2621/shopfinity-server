fa21a976bf3eb910e7d32789ff1e7c140af14a29
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-11-24 19:31:11 -0500

    added file

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error("🔥 ERROR:", err);
+
+  const status = err.statusCode || 500;
+
+  return res.status(status).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+};
