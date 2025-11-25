cecd8b9c78090403c4f5bd5231777dea730409c2
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-11-24 20:03:46 -0500

    added files

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -6,22 +6,40 @@ import {
   updateCategory,
   deleteCategory,
 } from "../controllers/categoryController";
+import validate from "../middleware/validateResource";
+
+import { createCategorySchema } from "../validations/category.validation";
+import { updateCategorySchema } from "../validations/category.validation";
+import { deleteCategorySchema } from "../validations/category.validation";
+import { getSingleCategorySchema } from "../validations/category.validation";
 
 const router = express.Router();
 
 //  Create
-router.post("/create-category", createCategory);
+router.post("/create-category", validate(createCategorySchema), createCategory);
 
 //  Get all
 router.get("/", getAllCategories);
 
 //  Get single
-router.get("/:id", getSingleCategory);
+router.get(
+  "/single-category/:id",
+  validate(getSingleCategorySchema),
+  getSingleCategory
+);
 
 //  Update
-router.put("/:id", updateCategory);
+router.put(
+  "/update-category/:id",
+  validate(updateCategorySchema),
+  updateCategory
+);
 
 //  Delete
-router.delete("/:id", deleteCategory);
+router.delete(
+  "/delete-category/:id",
+  validate(deleteCategorySchema),
+  deleteCategory
+);
 
 export default router;
