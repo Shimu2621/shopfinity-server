1495a8af203373f6296ce1ad7f1de22ad45df4ce
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-11-24 19:21:54 -0500

    added file

--- src/validations/category.validation.ts
+++ src/validations/category.validation.ts
@@ -5,11 +5,37 @@ export const createCategorySchema = z.object({
   body: z.object({
     name: z.string().min(1, "Name is required"),
     icon: z.string().optional(),
+    slug: z.string(),
     description: z.string().optional(),
     parentId: z
       .string()
       .refine((val) => mongoose.isValidObjectId(val), "Invalid parentId")
       .nullable()
       .optional(),
+  }),
+});
+
+export const updateCategorySchema = z.object({
+  params: z.object({
+    id: z.string(),
+  }),
+  body: z.object({
+    name: z.string().optional(),
+    icon: z.string().optional(),
+    slug: z.string().optional(),
+    description: z.string().optional(),
+    parentId: z.string().optional(),
+  }),
+});
+
+export const getSingleCategorySchema = z.object({
+  params: z.object({
+    id: z.string(),
+  }),
+});
+
+export const deleteCategorySchema = z.object({
+  params: z.object({
+    id: z.string(),
   }),
 });
