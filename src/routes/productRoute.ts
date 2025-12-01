6c42d2f654326afaf40b3dd8882837f6cd31cc52
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-11-30 22:17:35 -0500

    changes doen

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -6,13 +6,18 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/productController";
+import validate from "../middleware/validateResource";
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../validations/product.validation";
 
 const router = express.Router();
 
-router.post("/create-product", createProduct);
-router.get("/products", getAllProducts);
+router.post("/create-product", validate(createProductSchema), createProduct);
+router.get("/", getAllProducts);
 router.get("/product/:id", getSingleProduct);
-router.put("/product/:id", updateProduct);
+router.put("/product/:id", validate(updateProductSchema), updateProduct);
 router.delete("/product/:id", deleteProduct);
 
 export default router;
