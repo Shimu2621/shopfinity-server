516f2a44847f8f9e7330f118be9252f4ac96bff4
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-12-02 22:26:53 -0500

    changes file

--- src/validations/review.validation.ts
+++ src/validations/review.validation.ts
@@ -6,6 +6,7 @@ export const createReviewSchema = z.object({
     comment: z.string().min(1, "Comment is required"),
     userId: z.string().min(1, "User ID is required"),
     productId: z.string().min(1, "Product ID is required"),
+    createdAt: z.date().optional(),
   }),
 });
 
