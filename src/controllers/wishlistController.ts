f7efe3d9acd33468f9e7474f26063fa19ba66867
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-12-27 22:38:49 -0500

    completed all

--- src/controllers/wishlistController.ts
+++ src/controllers/wishlistController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { WishlistModel } from "../models/wishlistModel";
+import { IWishlistQuery } from "../interfaces/wishlist.interface";
 
 // Add to wishlist
 export const createWishlist = async (req: Request, res: Response) => {
@@ -19,17 +20,40 @@ export const createWishlist = async (req: Request, res: Response) => {
 
 // Get user wishlist
 export const getWishlist = async (req: Request, res: Response) => {
-  const { userId, page = "1", limit = "10" } = req.query;
+  const {
+    userId,
+    page = "1",
+    limit = "10",
+    searchTerm,
+  } = req.query as IWishlistQuery;
 
   const skip = (Number(page) - 1) * Number(limit);
 
-  const data = await WishlistModel.find({ userId })
+  // Build base filter
+  const filters: any = {};
+  if (userId) filters.userId = userId;
+
+  // Fetch data with populated products
+  let query = WishlistModel.find(filters)
     .populate("productId")
     .skip(skip)
     .limit(Number(limit));
 
+  // If searchTerm exists, filter by product name
+  if (searchTerm) {
+    query = query.where("productId.name", new RegExp(searchTerm, "i"));
+  }
+
+  const data = await query;
+  const total = await WishlistModel.countDocuments(filters);
+
   res.status(200).json({
     success: true,
+    meta: {
+      page: Number(page),
+      limit: Number(limit),
+      total,
+    },
     data,
   });
 };
