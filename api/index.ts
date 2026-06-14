3ac63e3f7b2fa3cd7f26c2fe598bd7a7bca3dee8
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-14 10:33:01 -0400

    Test vercel function

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,11 @@
-import serverless from "serverless-http";
-import app from "../src/app";
+// import serverless from "serverless-http";
+// import app from "../src/app";
 
-export const handler = serverless(app);
+export default function handler(req: any, res: any) {
+  res.status(200).json({
+    success: true,
+    message: "Vercel function works",
+  });
+}
+
+// export const handler = serverless(app);
