b570d1b4cd74268b4a1b69ffedc89716e11f5027
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-14 10:44:27 -0400

    Fix Vercel default export

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,6 @@
-// import serverless from "serverless-http";
-// import app from "../src/app";
+import serverless from "serverless-http";
+import app from "../src/app";
 
-export default function handler(req: any, res: any) {
-  res.status(200).json({
-    success: true,
-    message: "Vercel function works",
-  });
-}
+const handler = serverless(app);
 
-// export const handler = serverless(app);
+export default handler;
