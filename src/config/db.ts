9472228880ceea03c86d12f9c294a60daa052b86
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-14 20:34:30 -0400

    update config file

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,9 @@ const MONGO_URI = process.env.MONGO_URI as string;
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", (error as Error).message);
