bcead125e904b53797da163b117c203bd8fe0d9e
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-20 22:11:46 -0400

    update product file

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -53,7 +53,7 @@ export const getAllProducts = async (
       .limit(limitNumber)
       .skip(skip);
 
-    // console.log("STEP 3");
+    console.log("STEP 3");
 
     // ✅ Map categoryId and brandId to category and brand in plain JS objects
     const mappedProducts = (products as IProductWithCategoryBrand[]).map(
