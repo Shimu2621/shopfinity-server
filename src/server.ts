0db922a18902d4c05ecf5ac0dae5e1a03fb4f8ad
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-21 23:33:03 -0400

    update server file

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,32 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db";
-import { errorHandler } from "./middleware/errorHandler";
+// import express, { Request, Response } from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import { connectDB } from "./config/db";
+// import { errorHandler } from "./middleware/errorHandler";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// const app = express();
+// const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  }),
-);
+// // Middleware
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   }),
+// );
 
-app.use("/api/payment/webhook", express.raw({ type: "application/json" }));
+// app.use("/api/payment/webhook", express.raw({ type: "application/json" }));
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
-// Connect to Database
-connectDB();
+// // Connect to Database
+// connectDB();
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// // Start server
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
