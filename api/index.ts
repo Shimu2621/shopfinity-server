38deea120990549e0c8ec15d58fbec1b8c587afa
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-21 23:25:33 -0400

    update index file

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,21 @@
-import serverless from "serverless-http";
-import app from "../src/app";
-import { connectDB } from "../src/config/db";
+// import serverless from "serverless-http";
+// import app from "../src/app";
+// import { connectDB } from "../src/config/db";
 
-let isConnected = false;
+// let isConnected = false;
 
-const serverlessHandler = serverless(app);
+// const serverlessHandler = serverless(app);
 
-const handler = async (req: any, res: any) => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-  } catch (err) {
-    console.error("DB connection error:", err);
-  }
-  return serverlessHandler(req, res);
-};
+// const handler = async (req: any, res: any) => {
+//   try {
+//     if (!isConnected) {
+//       await connectDB();
+//       isConnected = true;
+//     }
+//   } catch (err) {
+//     console.error("DB connection error:", err);
+//   }
+//   return serverlessHandler(req, res);
+// };
 
-export default handler;
+// export default handler;
