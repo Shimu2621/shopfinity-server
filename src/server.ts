751c4b845080a40ec28218e7615029b26d5b56ed
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-06-27 22:35:44 -0400

    update file

--- src/server.ts
+++ src/server.ts
@@ -31,17 +31,25 @@
 //   console.log(`🚀 Server running on http://localhost:${PORT}`);
 // });
 
-import app from "./app";
 import dotenv from "dotenv";
+import app from "./app";
 import { connectDB } from "./config/db";
 
 dotenv.config();
 
-// connect DB only for local dev
-connectDB();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
